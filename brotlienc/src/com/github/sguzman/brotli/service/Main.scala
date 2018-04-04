3de4dfee5fdf6f8d392e396f2b408b908a774f29
Salvador Guzman <guzmansalv@gmail.com>
2018-04-03 18:56:12 -0700

    add basic model

--- brotlienc/src/com/github/sguzman/brotli/service/Main.scala
+++ brotlienc/src/com/github/sguzman/brotli/service/Main.scala
@@ -1,6 +1,11 @@
 package com.github.sguzman.brotli.service
 
+import scala.collection.mutable
+
 object Main {
+  final case class Model(url: String, isBrotli: Boolean)
+  val urls: mutable.HashSet[Model] = mutable.HashSet()
+
   def main(args: Array[String]): Unit = {
     println("Hello world")
   }
