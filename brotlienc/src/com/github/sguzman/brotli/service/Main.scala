1a115ff60868074c7efed7dc4a666c0b5ed13ee8
Salvador Guzman <guzmansalv@gmail.com>
2018-04-03 20:45:51 -0700

    working use of circe on github json body

--- brotlienc/src/com/github/sguzman/brotli/service/Main.scala
+++ brotlienc/src/com/github/sguzman/brotli/service/Main.scala
@@ -2,7 +2,7 @@ package com.github.sguzman.brotli.service
 
 import java.net.URL
 
-import com.github.sguzman.brotli.service.upload.Upload
+import com.github.sguzman.brotli.service.protoc.upload.Upload
 import lol.http.{Server, _}
 import org.apache.commons.lang3.StringUtils
 import scalaj.http.Http
@@ -25,24 +25,29 @@ object Main {
     }
 
     Server.listen(port) {req =>
-      req.readAs[String].map {url =>
-        scribe.info(s"Received: $url")
-        val obj = new URL(url)
-        req.method match {
-          case HttpMethod("PUT") =>
-            scribe.info(s"Path ${obj.getPath}")
+      util.Try{
+        req.readAs[String].map { url =>
+          scribe.info(s"Received: $url")
+          val obj = new URL(url)
+          req.method match {
+            case HttpMethod("PUT") =>
+              scribe.info(s"Path ${obj.getPath}")
 
-            val user = obj.getPath.stripPrefix("/").before("/")
-            scribe.info(s"User $user")
+              val user = obj.getPath.stripPrefix("/").before("/")
+              scribe.info(s"User $user")
 
-            val repo = obj.getPath.stripPrefix(s"/$user/").before("/")
-            scribe.info(s"Repo $repo")
+              val repo = obj.getPath.stripPrefix(s"/$user/").before("/")
+              scribe.info(s"Repo $repo")
 
-            val exists = Http(s"https://api.github.com/repos/$user/$repo/commits/master").asString
-            Ok(exists.body)
-          case _ => NotFound
+              val exists = Http(s"https://api.github.com/repos/$user/$repo/commits/master").asString
+              Ok(exists.body)
+            case _ => NotFound
+          }
         }
-      }
+      } match {
+          case Success(v) => v
+          case Failure(_) => InternalServerError
+        }
     }
 
     println("Listening on http://localhost:8888...")
